700899b2003a0c1cd07a0de1d97e37276bf814f4
unknown <Ogiramma@ISOTEL.ads.ssc.wisc.edu>
2015-12-04 15:18:26 -0600

    Adding scripts
    I am adding these scripts to the repository

---
+++ time.py
@@ -0,0 +1,31 @@
+from sys import exit
+
+#This module tracks and advances time as activities are done.
+
+class Time(object):
+
+	def __init__(self):
+		self.day = 1
+		self.current_time = 8
+		print self.day
+		print self.current_time
+
+#This function advances the time of day by the number of specified hours.
+#In the even that an activity takes more than one, it can accomidate.
+	def advance_tod(self, hours):  
+		self.current_time = self.current_time + hours
+		#print self.current_time
+
+#This should advance the player to the next day and set the time to be 8 again.
+#Need to test in actual system.
+	def advance_day(self):
+		if self.current_time >= 21:
+			self.day = self.day + 1
+			self.current_time = 8
+
+
+x = Time()
+x.advance_tod(12)
+x.advance_day()
+print x.day
+print x.current_time
